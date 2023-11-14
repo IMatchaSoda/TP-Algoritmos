dec99b17e4bcdced1eb1b3bc27b661d927287277
IMatchaSoda <arliajuansebastian@gmail.com>
2023-11-14 14:37:59 -0300

    Update Grupo_Obrero.cs

--- Grupo_Obrero.cs
+++ Grupo_Obrero.cs
@@ -24,5 +24,46 @@ namespace TPAlgoritmos_Constructora
 		public int ID_Grupo{
 			get{return id_Grupo;}set{id_Grupo=value;}
 		}
+		public ArrayList Listaobreros{
+			get{return listaObreros;}
+		}
+		//crea una instancia de obrero, y la añade a la lista
+		public void Agregar_Obrero(string nombre,string apellido,string cargo,double sueldo,int legajo,int dni){
+			Obra Nuevo_obrero= new Obrero(nombre,apellido,cargo,sueldo,legajo,dni);
+			listaObras.Add(Nuevo_obrero);
+		}
+		//muestra la cantidad de elementos en lista
+		public void cantidad_Obreros(){
+			listaObreros.Count();
+		}
+		//itera sobre lista,buscando en base a DNI, si existe=true,lo encontró
+		public void buscar_Obrero(int dni){ 
+			bool encontrado= false;
+			foreach(Obrero obrero in listaObreros){
+				if(obrero.DNI==dni){
+					encontrado=true;
+					Console.WriteLine("Obrero encontrado");
+					break;
+				}
+			}
+		}
+		//itera sobre la lista de obreros,y llama al metodo imprimir
+		public void mostrar_Obreros(){
+			for(int i=0;i<listaObreros.Count;i++){
+				listaObreros[i].imprimir();
+			}
+		}
+		//itera sobre lista,buscando en base a DNI, si existe=true, elimina en indice
+		public void eliminar_Obrero(int dni){ 
+			bool existe=false;
+			for(int i=0;i<listaObras.Count;i++){
+				if(listaObreros[i].DNI==dni){
+					existe=true;
+					listaObreros.RemoveAt(i);
+					break;
+				}	
+			}
+
+		}
 	}
 }
