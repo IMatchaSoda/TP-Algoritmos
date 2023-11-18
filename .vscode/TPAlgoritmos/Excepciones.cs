2ced411ba8016010f69558f47319260a5263a257
IMatchaSoda <arliajuansebastian@gmail.com>
2023-11-18 17:26:13 -0300

    Create Excepciones.cs

---
+++ .vscode/TPAlgoritmos/Excepciones.cs
@@ -0,0 +1,17 @@
+using System;
+using TPAlgoritmos_Constructora;
+
+
+using System.Collections;
+namespace TPAlgoritmos_Constructora
+{
+    public class nohayobradisponible:Exception
+    {
+        private string message;
+        public nohayobradisponible(string message):base(){
+            
+        }
+
+
+    }
+}
